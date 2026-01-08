3e7a4ddc145b82a6e5ed6d0090e10ab6c963eeed
Michael Bagnato <mbagnato@agl.com.au>
2026-01-08 13:13:09 +1100

    Initial upload

---
+++ weather.py
@@ -0,0 +1,13 @@
+from fastmcp import FastMCP
+from json import loads
+from urllib.request import urlopen
+
+mcp = FastMCP("Weather")
+
+@mcp.tool
+def getWeather(city):
+    response = urlopen(f"http://wttr.in/{city}?format=j2").read()
+    weatherData = loads(response)
+    return { "temperatureC": weatherData["current_condition"][0]["temp_C"] }
+
+mcp.run(transport = "http", port=9000)
